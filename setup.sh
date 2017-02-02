d2186ab9875786ca6af733df02f91de436a51aec
Lukas <lftkv@users.noreply.github.com>
2017-02-02 14:45:27 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -320,7 +320,7 @@ setGitAliase()
     git config --global alias.alias "!git config -l | grep alias | cut -c 7-"
     git config --global alias.plog "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'"
     git config --global alias.lg "log --color --graph --pretty=format:'%C(red)%h%Creset -%C(yellow)%d%Creset %s %Cred(%cr) %C(cyan)<%an>%Creset' --abbrev-commit"
-    git config --global alias.upgrade "-c 'source <(curl -s https://raw.githubusercontent.com/lftkv/git/master/setup.sh) -u'"
+    git config --global alias.upgrade "!bash -c 'source <(curl -s https://raw.githubusercontent.com/lftkv/git/master/setup.sh) -u'"
     git config --global alias.yolo "push -f"
 }
 
